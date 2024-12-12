4d7c65a3fbd3d24ef11732ce152fb8dc82e6c89a
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2024-12-12 09:55:26 -0500

    sync

--- 2024/cmd/day03/main.go
+++ 2024/cmd/day03/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	content, err := os.ReadFile("input/day03.txt")
-	// content, err := os.ReadFile("input/test.txt")
+	// content, err := os.ReadFile("input/day03.txt")
+	content, err := os.ReadFile("input/test.txt")
     if err != nil { panic(err) }
-	part1(string(content))
+	part2(string(content))
 }
 
 func part1(str string) {
@@ -22,7 +22,7 @@ func part1(str string) {
 	sequence := 0
 	i := 0
 	for i < len(str) {
-		if sequence == 7 && str[i] == ')' {
+		if sequence == len(sample) - 1 && str[i] == sample[len(sample) - 1] {
 			sum += a * b
 			sequence = 0
 		}
@@ -46,7 +46,7 @@ func part1(str string) {
 				b = val
 			}
 			sequence++
-			continue
+			continue 
 		}
 		if str[i] == sample[sequence] {
 			sequence++
@@ -58,12 +58,61 @@ func part1(str string) {
 	fmt.Println(sum)
 }
 
-func Pow(x, n int) int {
-   if n == 0 { return 1 }
-   if n == 1 { return x }
-   y := Pow(x, n/2)
-   if n % 2 == 0 { return y*y }
-   return x*y*y
+func part2(str string) {
+	samples := []string {"mul(a,b)", "do()", "don't()"}
+	sequences := []int {0, 0, 0}
+	sum := 0
+	greenLight := true
+	a := 0
+	b := 0
+	for i := 0; i < len(str); {
+		for j := 0; j < 3; j++ {
+			if sequences[j] == len(samples[j]) - 1 && str[i] == samples[j][len(samples[j]) - 1] {
+				sequences[j] = 0
+				// fmt.Printf("A: %d, B: %d\n", a, b)
+				if greenLight {
+					fmt.Printf("greenLight is active\n")
+				} else {
+					fmt.Printf("greenLight is NOT active\n")
+				}
+				if j == 0 && greenLight { sum += a * b }
+				if j == 1 { greenLight = true }
+				if j == 2 { greenLight = false }
+			}
+		}
+		if sequences[0] == 4 || sequences[0] == 6 {
+			strVal := ""
+			j := 0
+			if !unicode.IsDigit(rune(str[i])) {
+				sequences[0] = 0
+				i++
+				continue
+			}
+			for unicode.IsDigit(rune(str[i])) && j < 3 {
+				strVal += string(str[i])
+				j++
+				i++
+			}
+			val, _ := strconv.Atoi(strVal)
+			if sequences[0] == 4 {
+				a = val
+			} else {
+				b = val
+			}
+			sequences[0]++
+			continue 
+		}
+		for j := 0; j < 3; j++ {
+			if str[i] == samples[j][sequences[j]] {
+				sequences[j]++
+			} else {
+				sequences[j] = 0
+			}
+		}
+		fmt.Printf("Current index : %d, current char %s, sequence: %d\n", i, string(str[i]), sequences[0])
+		i++
+	}
+	fmt.Println(sum)
 }
 // func part2(nums [][]int) {
 //
