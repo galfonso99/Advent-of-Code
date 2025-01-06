bc2aa2b4ff9bbe42b39625796ccb97eed605b639
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2025-01-06 02:21:03 -0500

    Finished day09 part 2

--- 2024/cmd/day09/main.go
+++ 2024/cmd/day09/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	nums := parseInput()
-	part1(nums)
+	part2(nums)
 }
 
 func parseInput() []int {
@@ -18,7 +18,6 @@ func parseInput() []int {
 	if err != nil { panic(err) }
 	trimmed := strings.TrimRight(string(content), "\n")
 	split := strings.Split(trimmed, "")
-	fmt.Println(len(split))
 	nums := make([]int, len(split))
 	for i, digit := range split {
 		nums[i], _ = strconv.Atoi(digit)
@@ -67,7 +66,6 @@ func part1(nums []int) {
 		// fmt.Println(memoryMap)
 		x++
 	}
-	// fmt.Println(memoryMap)
 	for i, num := range memoryMap {
 		if sum > 0 && num == 0 { break }
 		sum += i * num
@@ -81,7 +79,59 @@ func fillRange(slice []int, start int, end int, val int) {
 	}
 }
 
+// Instead of keeping track of empty indices keep an array of 2 sized arrays with the starting index
+// and the size of the space
+// For each file see if there is a space where it could fit and put it there, if not then leave it
 func part2(nums []int) {
-	
+	i := 0
+	sum := 0
+	numSum := 0
+	for i := 0; i < len(nums); i++ {
+		numSum += nums[i]
+	}
+	memoryMap := make([]int, numSum)
+	emptyAreas := make([][2]int, 0, 5)
+	fileGroups := make([][2]int, 0, 5)
+	for j := 0; j < len(nums); j++ {
+		if j % 2 == 0 {
+			fillRange(memoryMap, i, i + nums[j], j / 2)
+			fileGroups = append(fileGroups, [2]int {i, nums[j]} )
+		} else if j % 2 == 1 && nums[j] != 0 {
+			emptyAreas = append(emptyAreas, [2]int {i, nums[j]})
+		}
+		i += nums[j]
+	}
+	for i := len(fileGroups) - 1; i >= 0; i-- {
+		areaIndex := findArea(fileGroups[i], emptyAreas)
+		if areaIndex == -1 {      // No area was found
+			continue
+		}
+		swapFilePosition(memoryMap, fileGroups[i], emptyAreas[areaIndex])
+		emptyAreas[areaIndex][0] += fileGroups[i][1]
+		emptyAreas[areaIndex][1] -= fileGroups[i][1]
+	}
+	// fmt.Println(memoryMap)
+	for i, num := range memoryMap {
+		sum += i * num
+	}
+	fmt.Println(sum)
+}
+
+func findArea(fileGroup [2]int, emptyAreas [][2]int) int {
+	// Loop through all the emptyAreas from the start and check if the size is enough and
+	// the index is before the file index
+	for i, emptyArea := range emptyAreas {
+		if emptyArea[1] >= fileGroup[1] && emptyArea[0] < fileGroup[0] {
+			return i
+		}
+	}
+	return -1
+}
+
+func swapFilePosition(memoryMap []int, fileGroup [2]int, emptyAreas [2]int) {
+	for i := 0; i < fileGroup[1]; i++ {
+		memoryMap[emptyAreas[0] + i] = memoryMap[fileGroup[0] + i]
+		memoryMap[fileGroup[0] + i] = 0
+	}
 }
 
