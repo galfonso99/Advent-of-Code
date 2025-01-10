d63c27af1c951a5a22c4ebc19a78f9c2039c36ac
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2025-01-09 22:25:58 -0500

    Solved day11 part 2 using recursion, DFS and memoization

--- 2024/cmd/day11/main.go
+++ 2024/cmd/day11/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 	"os"
 	"slices"
@@ -10,21 +9,21 @@ import (
 )
 
 func main() {
-	stringNums := parseInput()
-	part2(stringNums)
+	nums := parseInput()
+	part2(nums)
 }
 
-func parseInput() []string {
-	// content, err := os.ReadFile("input/day11.txt")
-	content, err := os.ReadFile("input/test.txt")
+func parseInput() []int {
+	content, err := os.ReadFile("input/day11.txt")
+	// content, err := os.ReadFile("input/test.txt")
     if err != nil { panic(err) }
 	trimmed := strings.TrimRight(string(content), "\n")
     stringNums := strings.Split(trimmed, " ")
-	// nums := make([]int, len(stringNums))
-	// for i, str := range stringNums {
-	// 	nums[i], _ = strconv.Atoi(str)
-	// }
-	return stringNums
+	nums := make([]int, len(stringNums))
+	for i, str := range stringNums {
+		nums[i], _ = strconv.Atoi(str)
+	}
+	return nums
 }
 // Starting with a string array of the original numbers for each blink:
 // - If the num is zero make it one
@@ -50,49 +49,49 @@ func part1(stringNums []string) {
 				i++
 			}
 		}
-		// fmt.Println(stringNums)
 	}
 	fmt.Println(len(stringNums))
 }
-func part2(stringNums []string) {
-	numsList := list.New()
-	for _, str := range stringNums {
-		numsList.PushBack(str)
+func part2(nums []int) {
+	sum := 0
+	memo := make(map[[2]int]int)
+	for _, num := range nums {
+		sum += howMany(&memo, num, 75)
 	}
-	for j := 0; j < 32; j++ {
-		numStr := numsList.Front()
-		for numStr != nil {
-			num, _ := strconv.Atoi(numStr.Value.(string))
-			if num == 0 {
-				numStr.Value = "1"
-			} else if len(numStr.Value.(string)) % 2 == 1 {
-				numStr.Value = strconv.Itoa(2024 * num)
-			} else {
-				// fmt.Println(numStr.Value.(string))
-				middle := len(numStr.Value.(string)) / 2
-				secondHalf := numStr.Value.(string)[middle:]
-				numStr.Value = numStr.Value.(string)[:middle]
-				// fmt.Println("Printing halves")
-				// fmt.Println(numStr.Value.(string)[:middle], secondHalf)
-				if len(secondHalf) > 1 { secondHalf = strings.TrimLeft(secondHalf, "0") }
-				if len(secondHalf) == 0 { secondHalf = "0" }
-				numsList.InsertAfter(secondHalf, numStr)
-				numStr = numStr.Next()
-			}
-			numStr = numStr.Next()
-		}
-		// for e := numsList.Front(); e != nil; e = e.Next() {
-		// 	fmt.Printf("%s ", e.Value)
-		// }
-		// fmt.Println()
-	}
-	fmt.Println(numsList.Len())
+	fmt.Println(sum)
 }
 
-// func replace(old *list.Element, new string) {
-// 	if old.Next() != nil {
-// 		next := old.Next()
-// 	} else {
-//
-// 	}
-// }
+func howMany(memo *map[[2]int]int, val int, blinks int) int {
+	if blinks == 0 {
+		return 1
+	} else {
+		if _, ok := (*memo)[[2]int {val, blinks}]; ok {
+			return (*memo)[[2]int {val, blinks}]
+		}
+		if val == 0 {
+			(*memo)[[2]int {val, blinks}] = howMany(memo, 1, blinks-1)
+			return (*memo)[[2]int {val, blinks}]
+		} else if getDigitCount(val) % 2 == 0 {
+			firstHalf, secondHalf := splitInHalf(val)
+			(*memo)[[2]int {val, blinks}] = howMany(memo, firstHalf, blinks-1) + howMany(memo, secondHalf, blinks-1)
+			return (*memo)[[2]int {val, blinks}]
+		} else {
+			(*memo)[[2]int {val, blinks}] = howMany(memo, val * 2024, blinks-1)
+			return (*memo)[[2]int {val, blinks}]
+		}
+	}
+}
+
+func getDigitCount(val int) int {
+	str := strconv.Itoa(val)
+	return len(str)
+}
+
+func splitInHalf(val int) (int, int) {
+	str := strconv.Itoa(val)
+	middle := len(str) / 2
+	firstStr, secondStr := str[:middle], str[middle:]
+	firstInt, _ := strconv.Atoi(firstStr)
+	secondInt, _ := strconv.Atoi(secondStr)
+	return firstInt, secondInt
+}
